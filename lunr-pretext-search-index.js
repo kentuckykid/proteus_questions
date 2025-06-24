2c0db0f7060cb3e31e87ca027c7e0fa7dfa07848
kentuckykid <stephenson@hope.edu>
2025-06-24 15:21:57 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -15,17 +15,26 @@ var ptx_lunr_docs = [
   "url": "sec-section-name.html",
   "type": "Section",
   "number": "1.1",
-  "title": "Section Title",
-  "body": " Section Title   Parsons Problem, Mathematical Proof  even numbers   Create a proof of the theorem: If is an even number, then .     Suppose is even.     Then is a prime number.    Then there exists an so that .    Then there exists an so that .     Click the heels of your ruby slippers together three times.    So .  This is a superfluous second paragraph in this block.    Thus .    And a little bit of irrelevant multi-line math .     Dorothy will not be much help with this proof.    "
+  "title": "PreTeXt Section 1.2 Exercises",
+  "body": " PreTeXt Section 1.2 Exercises   Do elementary row operations to achieve RREF   The following matrices each require one or a sequence of two EROs to convert them to RREF. For each matrix choose the ERO or sequence that will convert it to RREF. Some of the EROs or sequences of EROs in the list will not be used.                              then         then         then              then  then  then  then      Determine number of solutions from RREF.   Each of the following is the RREF of a linear system in augmented matrix form. The symbol represents an unknown real number. For each associated linear system, choose the correct response.           The linear system has one and only one solution.       The linear system has no solution.             The linear system has infinitely many solutions.       More information is needed to determine the number of solutions.     "
 },
 {
-  "id": "number-theory-proof",
+  "id": "ula_proteus_Section_1_2_RREF_using_EROs_v1",
   "level": "2",
-  "url": "sec-section-name.html#number-theory-proof",
+  "url": "sec-section-name.html#ula_proteus_Section_1_2_RREF_using_EROs_v1",
   "type": "Checkpoint",
   "number": "1.1.1",
-  "title": "Parsons Problem, Mathematical Proof.",
-  "body": " Parsons Problem, Mathematical Proof  even numbers   Create a proof of the theorem: If is an even number, then .     Suppose is even.     Then is a prime number.    Then there exists an so that .    Then there exists an so that .     Click the heels of your ruby slippers together three times.    So .  This is a superfluous second paragraph in this block.    Thus .    And a little bit of irrelevant multi-line math .     Dorothy will not be much help with this proof.   "
+  "title": "Do elementary row operations to achieve RREF.",
+  "body": " Do elementary row operations to achieve RREF   The following matrices each require one or a sequence of two EROs to convert them to RREF. For each matrix choose the ERO or sequence that will convert it to RREF. Some of the EROs or sequences of EROs in the list will not be used.                              then         then         then              then  then  then  then    "
+},
+{
+  "id": "ula_proteus_Section_1_2_Solutions_from_RREF_v1",
+  "level": "2",
+  "url": "sec-section-name.html#ula_proteus_Section_1_2_Solutions_from_RREF_v1",
+  "type": "Checkpoint",
+  "number": "1.1.2",
+  "title": "Determine number of solutions from RREF..",
+  "body": " Determine number of solutions from RREF.   Each of the following is the RREF of a linear system in augmented matrix form. The symbol represents an unknown real number. For each associated linear system, choose the correct response.           The linear system has one and only one solution.       The linear system has no solution.             The linear system has infinitely many solutions.       More information is needed to determine the number of solutions.    "
 },
 {
   "id": "backmatter-2",
